0ac70ccec78b516bc037efc1121bd8cbc2bc430f
Light161 <Vovabadashev@mail.ru>
2021-10-16 19:42:39 +0300

    3

---
+++ src/LttFlow.Application/SeedTimer/SeedTimerAppService.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using LttFlow.MeterReading;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LttFlow.SeedTimer
+{
+    public class SeedTimerAppService : LttFlowAppServiceBase, ISeedTimerAppService
+    {
+        private readonly IRepository<MeterReadingList, long> _meterReadingRepository;
+
+        public SeedTimerAppService(IRepository<MeterReadingList, long> meterReadingRepository)
+        {
+            _meterReadingRepository = meterReadingRepository;
+        }
+
+
+    }
+}
