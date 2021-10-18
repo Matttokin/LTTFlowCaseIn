3b39d9d0b0abcbdd5836d702f14c30ca152bec06
Жжонов Валерий Алексеевич <61112419+ValeriiL17@users.noreply.github.com>
2021-10-18 23:09:48 +0300

    График

--- src/LttFlow.Web.Mvc/wwwroot/view-resources/Views/Home/Index.js
+++ src/LttFlow.Web.Mvc/wwwroot/view-resources/Views/Home/Index.js
@@ -47,10 +47,59 @@ $(function () {
     var _meterReadingService = abp.services.app.meterReading;
 
     var salesChartCanvas = $('#salesChart').get(0).getContext('2d');
-    var salesChartCanvas1 = $('#salesChart').get(0).getContext('2d');
+    var gaseChartCanvas = $('#gaseChart').get(0).getContext('2d');
 
-    var salesChartCanvas3 = $('#salesChart').get(0).getContext('2d');
+    var gaseChartNewCanvas = $('#gaseChartNew').get(0).getContext('2d');
     // This will get the first returned node in the jQuery collection.
+
+    var salesChartOptions = {
+        //Boolean - If we should show the scale at all
+        showScale: true,
+        //Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines: false,
+        //String - Colour of the grid lines
+        scaleGridLineColor: 'rgba(0,0,0,.05)',
+        //Number - Width of the grid lines
+        scaleGridLineWidth: 1,
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+        //Boolean - Whether the line is curved between points
+        bezierCurve: true,
+        //Number - Tension of the bezier curve between points
+        bezierCurveTension: 0.3,
+        //Boolean - Whether to show a dot for each point
+        pointDot: false,
+        //Number - Radius of each point dot in pixels
+        pointDotRadius: 4,
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth: 1,
+        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+        pointHitDetectionRadius: 20,
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke: true,
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth: 2,
+        //Boolean - Whether to fill the dataset with a color
+        datasetFill: true,
+        //String - A legend template
+        legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].lineColor%>"></span><%=datasets[i].label%></li><%}%></ul>',
+        //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+        maintainAspectRatio: false,
+        //Boolean - whether to make the chart responsive to window resizing
+        responsive: true,
+
+        scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                    beginAtZero: true   // minimum value will be 0.
+                }
+            }]
+        }
+    };
 
     var salesChartData = {
         labels: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль'],
@@ -80,6 +129,11 @@ $(function () {
         ]
     };
 
+    var salesChart = new Chart(salesChartCanvas, {
+        type: 'line',
+        data: salesChartData
+    });
+
     _meterReadingService.getChartData().done(function (result) {
         var salesChartData = {
             labels: [],
@@ -101,58 +155,7 @@ $(function () {
 
         salesChartData.labels = removeDuplicates(salesChartData.labels);
 
-        var salesChartOptions = {
-            //Boolean - If we should show the scale at all
-            showScale: true,
-            //Boolean - Whether grid lines are shown across the chart
-            scaleShowGridLines: false,
-            //String - Colour of the grid lines
-            scaleGridLineColor: 'rgba(0,0,0,.05)',
-            //Number - Width of the grid lines
-            scaleGridLineWidth: 1,
-            //Boolean - Whether to show horizontal lines (except X axis)
-            scaleShowHorizontalLines: true,
-            //Boolean - Whether to show vertical lines (except Y axis)
-            scaleShowVerticalLines: true,
-            //Boolean - Whether the line is curved between points
-            bezierCurve: true,
-            //Number - Tension of the bezier curve between points
-            bezierCurveTension: 0.3,
-            //Boolean - Whether to show a dot for each point
-            pointDot: false,
-            //Number - Radius of each point dot in pixels
-            pointDotRadius: 4,
-            //Number - Pixel width of point dot stroke
-            pointDotStrokeWidth: 1,
-            //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-            pointHitDetectionRadius: 20,
-            //Boolean - Whether to show a stroke for datasets
-            datasetStroke: true,
-            //Number - Pixel width of dataset stroke
-            datasetStrokeWidth: 2,
-            //Boolean - Whether to fill the dataset with a color
-            datasetFill: true,
-            //String - A legend template
-            legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].lineColor%>"></span><%=datasets[i].label%></li><%}%></ul>',
-            //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-            maintainAspectRatio: false,
-            //Boolean - whether to make the chart responsive to window resizing
-            responsive: true,
-
-            scales: {
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        suggestedMin: 11,    // minimum will be 0, unless there is a lower value.
-                        // OR //
-                        suggestedMax: 17,
-                        beginAtZero: true   // minimum value will be 0.
-                    }
-                }]
-            }
-        };
-
-        var salesChart = new Chart(salesChartCanvas, {
+        var salesChart = new Chart(gaseChartCanvas, {
             type: 'bar',
             data: salesChartData,
             options: salesChartOptions
@@ -161,11 +164,10 @@ $(function () {
 
     var arrayfil = [];
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 4; i++) {
         var fiel = {
             groupName: 'Регион №' + getRandomInt(100),
-            value: i + getRandomInt(700),
-            date: i + ' октября',
+            date: i + ' октября'
         };
         arrayfil.push(fiel);
     }
@@ -175,26 +177,26 @@ $(function () {
         datasets: []
     };
 
-    var dataset = {
-        label: 'Приборы учета',
-        backgroundColor: [],
-        borderColor: [],
-        spanGaps: true,
-        data: []
-    };
-
-    arrayfil.forEach(datas => {
+    arrayfil.forEach(das => {
         var color = 'rgba(' + getRandomInt(255) + ', ' + getRandomInt(255) + ', ' + getRandomInt(255) + ', 1)';
-        dataset.backgroundColor.push(color);
-        dataset.borderColor.push(color);
-        dataset.data.push(datas.value);
-        salesChartData.labels.push(datas.groupName);
+        var dataset = {
+            label: das.groupName,
+            backgroundColor: color,
+            borderColor: color,
+            spanGaps: true,
+            fill: false,
+            data: []
+        };
+        for (var i = 0; i < 4; i++) {
+            dataset.data.push(getRandomInt(100));
+        }
+        salesChartData.labels.push(das.date);
+        salesChartData.datasets.push(dataset);
     });
 
-    var salesChart = new Chart(salesChartCanvas1, {
+    var salesChart = new Chart(gaseChartNewCanvas, {
         type: 'line',
         data: salesChartData,
         options: salesChartOptions
     });
-
 });
